731679e774144250ab68ef6d699b23fe081e252e
Fengkai Yang <blesseven@gmail.com>
2025-02-02 21:48:58 +0100

    chapter 4 p192

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Login settings
+LOGIN_REDIRECT_URL = "dashboard"
+LOGIN_URL = "login"
+LOGOUT_URL = "logout"
+
+# Email backend
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
