c9a28e34b787fa3e64c72fb20c3800de454b84b1
Fengkai Yang <blesseven@gmail.com>
2025-02-09 21:34:47 +0100

    p261

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,9 +1,15 @@
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+import requests
+
 from .models import Image
+
 
 class ImageCreateForm(forms.ModelForm):
     class Meta:
-        model=Image
+        model = Image
         fields = ["title", "url", "description"]
         widgets = {
             "url": forms.HiddenInput,
@@ -12,7 +18,26 @@ class ImageCreateForm(forms.ModelForm):
     def clean_url(self):
         url = self.cleaned_data["url"]
         valid_extensions = ["jpg", "jpeg", "png"]
-        extension = url.rsplit(".",1)[1].lower()
+        extension = url.rsplit(".", 1)[1].lower()
         if extension not in valid_extensions:
-            raise forms.ValidationError("The given URL does not match valid image extensions.")
+            raise forms.ValidationError(
+                "The given URL does not match valid image extensions."
+            )
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image: Image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        name = slugify(image.title)
+        extension = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extension}"
+        # download image from the given url
+        response = requests.get(image_url)
+        image.image.save(
+            image_name,
+            ContentFile(response.content),
+            save=False,
+        )
+        if commit:
+            image.save()
+        return image
